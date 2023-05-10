192be1c7393b6eaa10d1bf0a26cbce194ec18531
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2023-05-10 05:25:16 -0300

    Implement Form component from Cloudscape

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,26 +1,56 @@
-import Input from "@cloudscape-design/components/input";
-import { useState } from "react";
+import {
+  Button,
+  Container,
+  Form,
+  FormField,
+  Header,
+  Input,
+  SpaceBetween,
+} from "@cloudscape-design/components";
+import { FormEvent, useState } from "react";
 
 export default function LoginForm() {
-  
-const [username, setUsername] = useState("");
-const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(username, password);
+  }
 
   return (
-    <form>
-      <Input
-        placeholder="Usuário"
-        type="text"
-        value={username}
-        onChange={({ detail }) => setUsername(detail.value)}
-      />
-      <Input
-        placeholder="Senha"
-        type="password"
-        value={password}
-        onChange={({ detail }) => setPassword(detail.value)}
-      />
-      <button type="submit">Login</button>
+    <form onSubmit={handleSubmit}>
+      <Form
+        actions={
+          <SpaceBetween direction="horizontal" size="xs">
+            <Button formAction="none" variant="link">
+              Problemas para logar?
+            </Button>
+            <Button variant="primary">Login</Button>
+          </SpaceBetween>
+        }
+      >
+        <Container header={<Header variant="h2">Fazer login</Header>}>
+          <SpaceBetween direction="vertical" size="l">
+            <FormField label="Usuário">
+              <Input
+                placeholder="Usuário"
+                type="text"
+                value={username}
+                onChange={({ detail }) => setUsername(detail.value)}
+              />
+            </FormField>
+            <FormField label="Senha">
+              <Input
+                placeholder="Senha"
+                type="password"
+                value={password}
+                onChange={({ detail }) => setPassword(detail.value)}
+              />
+            </FormField>
+          </SpaceBetween>
+        </Container>
+      </Form>
     </form>
   );
 }
