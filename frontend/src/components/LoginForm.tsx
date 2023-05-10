9a0cd4f51b888361750bab5f9f0e5dd52bd8aee6
Gabriel Malosto <gabriel.malosto@estudante.ufjf.br>
2023-05-10 04:22:13 -0300

    Use first Cloudscape component

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,1 +1,26 @@
-export default function LoginForm() {}
+import Input from "@cloudscape-design/components/input";
+import { useState } from "react";
+
+export default function LoginForm() {
+  
+const [username, setUsername] = useState("");
+const [password, setPassword] = useState("");
+
+  return (
+    <form>
+      <Input
+        placeholder="Usuário"
+        type="text"
+        value={username}
+        onChange={({ detail }) => setUsername(detail.value)}
+      />
+      <Input
+        placeholder="Senha"
+        type="password"
+        value={password}
+        onChange={({ detail }) => setPassword(detail.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
